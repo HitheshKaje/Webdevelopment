0938d093fd0a810a84eb009e444cef5c8c2b72ec
Hithesh <hitheshkajmoole2004@gmail.com>
2026-04-03 12:32:29 +0530

    created the notes app with the help of code with harry

---
+++ projectreact/notes app/src/App.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import Nabvar from './components/Nabvar'
+import Card from './Card'
+function App() {
+  const [notes, setnotes] = useState([])
+  const [currentNote, setcurrentNote] = useState({ title: "", desc: "" })
+  useEffect(() => {
+    console.log(" i am useiffect")
+    let localnotes = localStorage.getItem("notes")
+    if (localnotes) {
+      setnotes(JSON.parse(localnotes))
+    }
+  }, [])
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setnotes([...notes, currentNote])
+    localStorage.setItem("notes",JSON.stringify([...notes, currentNote]))
+
+    
+  }
+  const deleteNote=(title)=>{
+    setNotes(notes.filter(item=>item.title!=title))
+    localStorage.setItem("notes", JSON.stringify(notes.filter(item => item.title != title)))
+
+  }
+  const handleChange = (e) => {
+    setcurrentNote({ ...currentNote, [e.target.name]: e.target.value })
+  }
+
+  return (
+    <>
+      <Nabvar />
+      <div className="full">
+
+        <main>
+          <h1>Create Your form</h1>
+          <form action="" onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="title">Title</label>
+
+              <input type="text" name="title" id="title"
+                value={currentNote.title} onChange={handleChange} />
+            </div>
+            <div>
+              <label htmlFor="desc">Description</label>
+              <textarea name="desc" id="desc" onChange={handleChange} value={currentNote.desc}></textarea>
+            </div>
+            <button>Submit</button>
+          </form>
+        </main>
+        <section>
+          <h2>Your Notes</h2>
+          <div className="container">
+
+            {notes && notes.map(note => {
+              return <Card key={note.title} deleteNote={deleteNote} desc={note.desc} />
+            })}
+          </div>
+        </section>
+
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ projectreact/notes app/src/components/Nabvar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Nabvar = () => {
+    return (
+        <nav>
+            <div className="logo">NOTEX</div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>logout</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nabvar
